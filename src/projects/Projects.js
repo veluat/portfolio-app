3f91533e6ba4d6202a354835b7fb08636a6fd2f9
Julia <veluat@gmail.com>
2023-06-29 21:15:19 +0300

    CSS fixed / Part 3

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -21,11 +21,11 @@ export const Projects = () => {
                     <div className={s.projects}>
                         <div className={s.projectGrid}>
                             <Project style={todolist} projectTitle={'TO-DO list'}
-                                     projectDescription={`Create, edit and delete TO-DO lists, manage tasks that can be customized for you. Used universal components, React, Redux Toolkit, ReduxThunk, Axios, TS, Formik, Material UI, Axios, ReduxThunk and other`}/>
+                                     projectDescription={`Create, edit and delete yours TO-DO lists, manage tasks that can be customized for you. React project with Redux Toolkit, TypeScript, Material UI, Axios, formik`}/>
                         </div>
                         <div className={s.projectGrid}>
                             <Project style={socialNetwork} projectTitle={'Social network'}
-                                     projectDescription={`A social network that implements adding and deleting friends, authorization, sending messages etc. Used class and functional components, React, Redux, ReduxThunk, Axios, Redux-Form and other libraries`}/>
+                                     projectDescription={`A social network that implements adding and deleting friends, authorization, sending messages etc. React project with Redux, Axios, TypeScript, React Hook Form`}/>
                         </div>
                     </div>
                 </div>
