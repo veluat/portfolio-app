e81944ca44a7a64b520ca9b06456ad3b7658728a
Julia <veluat@gmail.com>
2023-09-04 21:49:09 +0300

    fix: cv download

--- src/header/nav/Nav.js
+++ src/header/nav/Nav.js
@@ -12,7 +12,7 @@ export const Nav = () => {
                 <LinkPadding section={"Projects"} path={"projects"} activeClass={s.active}/>
                 <LinkPadding section={"Remote work"} path={"remoteWork"} activeClass={s.active}/>
                 <LinkPadding section={"Contacts"} path={"contacts"} activeClass={s.active}/>
-                <a className={s.resume} href={cv} target={'_blank'} rel="noreferrer">Download resume</a>
+                <a className={s.resume} href={cv} download target={'_blank'} rel="noreferrer">Download resume</a>
             </div>
         </div>
     )
