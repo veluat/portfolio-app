e5f7a341f0e81cb560935ede3ee074bc9add3e15
Julia <veluat@gmail.com>
2023-10-25 21:11:29 +0300

    fix: tag a

--- src/common/components/socialNetBlock/SocialNetBlock.js
+++ src/common/components/socialNetBlock/SocialNetBlock.js
@@ -10,23 +10,23 @@ export const SocialNetBlock = () => {
     return (
         <div className={s.boxContainer}>
             <div className={s.box}>
-                <a href="https://t.me/veluat" target="_blank">
-                    <img className={s.icon} src={telegram} alt={''}></img>
+                <a href="https://t.me/veluat" target="_blank" rel="noreferrer">
+                    <img className={s.icon} src={telegram} alt={'telegram'}></img>
                 </a>
             </div>
             <div className={s.box}>
-                <a href="https://github.com/veluat" target="_blank">
-                    <img className={s.icon} src={github} alt={''}></img>
+                <a href="https://github.com/veluat" target="_blank" rel="noreferrer">
+                    <img className={s.icon} src={github} alt={'github'}></img>
                 </a>
             </div>
             <div className={s.box}>
-                <a href="https://www.linkedin.com/in/julia-popova-developer/" target="_blank">
-                    <img className={s.icon} src={linkedin} alt={''}></img>
+                <a href="https://www.linkedin.com/in/julia-popova-developer/" target="_blank" rel="noreferrer">
+                    <img className={s.icon} src={linkedin} alt={'linkedin'}></img>
                 </a>
             </div>
             <div className={s.box}>
-                <a href="https://www.codewars.com/users/veluat" target="_blank">
-                    <img className={s.icon} src={codewars} alt={''}></img>
+                <a href="https://www.codewars.com/users/veluat" target="_blank" rel="noreferrer">
+                    <img className={s.icon} src={codewars} alt={'codewars'}></img>
                 </a>
             </div>
         </div>
